53694530346a3ec766f03b43056d7758f5396d8f
connorvm <connormannion24@yahoo.com>
2022-09-02 18:01:19 -0400

    commit tree a0824b791b117eff2fa1f2043268c8770268c106

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(name) {
+   const greeting = 'Hi, my name is ' + name +'.';
+    return(greeting); 
+}
+function introductionWithLanguage(name, language) {
+    return ('Hi, my name is ' + name +' and I am learning to program in ' + language +'.')
+} 
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    return ('Hi, my name is ' + name +' and I am learning to program in ' + language +'.')
+}
+
+   
+    
